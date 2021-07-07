fb3ecd0923d445cd10b97123d6d713972841b259
Hen D Ar <48202728+ARnamaku@users.noreply.github.com>
2021-07-07 11:00:22 +0800

    ini controler/soal.php sama views/soal/tambah_soal.php
    ini controler/soal.php sama views/soal/tambah_soal.php

--- application/views/soal/tambah_soal.php
+++ application/views/soal/tambah_soal.php
@@ -21,9 +21,9 @@
   <!-- Default box -->
   <div class="card">
              <div class="card-header">
-                
+
                 <!-- <div class="pull-right">
-                    <a href="<?=site_url('soal/tambah_soal')?>" class="float-right btn btn-primary btn-flat" >
+                    <a href="" class="float-right btn btn-primary btn-flat" >
                         <i class="fa fa-user-plus"></i> Create
                     </a>
                 </div> -->
@@ -31,30 +31,30 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body"> 
-              <form>
+              <?= form_open_multipart(soal/tambah_soal);?>
               <div class="card-body">
                 <div class="form-group">
-                  <label for="exampleInputnama">Nama Matkul</label>
-                  <input type="text" class="form-control" id="exampleInputnama" placeholder="masukan nama matkul">
+                  <label for="namamatkul">Nama Matkul</label>
+                  <input type="text" class="form-control" id="namamatkul" placeholder="masukan nama matkul">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputsemester">Semester</label>
-                  <input type="number" class="form-control" id="exampleInputsemester" placeholder="masukan semester">
+                  <label for="semester">Semester</label>
+                  <input type="number" class="form-control" id="semester" placeholder="masukan semester">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputtanggal">Tanggal Upload</label>
-                  <input type="date" class="form-control" id="exampleInputtanggal" placeholder="masukan tanggal upload">
+                  <label for="tanggal">Tanggal Upload</label>
+                  <input type="date" class="form-control" id="tanggal" placeholder="masukan tanggal upload">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputdosen">Dosen Pengampu</label>
-                  <input type="text" class="form-control" id="exampleInputdosen" placeholder="masukan nama dosen pengampu">
+                  <label for="dosen">Dosen Pengampu</label>
+                  <input type="text" class="form-control" id="dosen" placeholder="masukan nama dosen pengampu">
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputtahun">Tahun Ujian</label>
-                  <input type="text" class="form-control" id="exampleInputtahun" placeholder="masukan tahun ujian">
+                  <label for="tahun">Tahun Ujian</label>
+                  <input type="text" class="form-control" id="tahun" placeholder="masukan tahun ujian">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Kategori Soal</label>
+                    <label for="kategori">Kategori Soal</label>
                     <br>
                     <input type="radio" id="UTS" name="kategori" value="satu">
                     <label for="satu">Ujian Tengah Semester (UTS)</label>
@@ -62,11 +62,11 @@
                     <label for="dua">Ujian Akhir Semester (UAS)</label>
                   </div>
                 <div class="form-group">
-                  <label for="exampleInputFile">File Soal</label>
+                  <label for="file">File Soal</label>
                   <div class="input-group">
                     <div class="custom-file">
-                      <input type="file" class="custom-file-input" id="exampleInputFile">
-                      <label class="custom-file-label" for="exampleInputFile">masukan file soal</label>
+                      <input type="file" class="custom-file-input" name="file">
+                      <label class="custom-file-label" for="file">masukan file soal</label>
                     </div>
                   </div>
                 </div>
@@ -74,11 +74,13 @@
               <!-- /.card-body -->
 
               <!-- <div class="card-footer"></div> -->
-              <a href="#" class="btn btn-primary btn-block"
+             <button type="submit" class="btn btn-primary">tambah soal</button>
+             
+              <!-- <a href="#" class="btn btn-primary btn-block"
               ><b>Tambah Soal</b></a
-            >
+            > -->
               </div>
-            </form>
+            <?php echo form_close(); ?>
           
   </div>
   <!-- /.card -->
