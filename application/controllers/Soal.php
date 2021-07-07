ae1c4b3968f4d523b75115d0ddc3a9cc9320d49e
Randisyustico <52877998+Healer-ID@users.noreply.github.com>
2021-07-07 20:09:21 +0700

    update controler

--- application/controllers/Soal.php
+++ application/controllers/Soal.php
@@ -79,51 +79,6 @@ class Soal extends CI_Controller {
           </button></div>');
         $this->output->set_output(json_encode($msg));
 
-    public function tambah_Soal(){
-
-			$config['upload_path']	= './views/file';
-			$config['allow_types']	= 'pdf|jpg|png|doc';
-
-			$this->load->library('upload', $config);
-			if(!$this->upload->do_upload('userfile')){
-				echo "gagal memuat";
-
-			}else{
-				$file = $this->upload->data();
-				$file = $file['file_name'];
-				$namamatkul	= $this->input->post('namamatkul', TRUE);
-				$semester	= $this->input->post('semester', TRUE);
-				$tanggal	= $this->input->post('tanggal', TRUE);
-				$dosen		= $this->input->post('dosen', TRUE);
-				$tahun		= $this->input->post('tahun', TRUE);
-				$kategori	= $this->input->post('kategori', TRUE);
-
-				$data = array(
-					'namamatkul'	=> $namamatkul,
-					'semester'		=> $semester,
-					'tanggal'		=> $tanggal_upload,
-					'dosen'			=> $nama_dosen,
-					'tahun'			=> $tahun_ujian,
-					'kategori'		=> $id_kategori,
-					'file'			=> $file,
-				);
-				
-				$this->db->insert('soal', $data);
-				$this->session->set_flashdata('pesan', '<div
-				class="alert alert-succes" role="alert"berhasil</div> ');
-				redirect('soal');
-			}
-
-        $query = $this->soal->get();
-		$data = array(
-			'judul' => 'Soal',
-			'row' => $query->result(),
-		);
-		$this->template->load('template', 'soal/tambah_soal', $data);
-
+    
     }
 }
-// 	public function input_soal(){
-		
-   
-// }
