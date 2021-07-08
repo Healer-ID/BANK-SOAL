6e1de57967f0250302154d6f9e21fb92bfed1a98
Andrean Farhantama <48090860+ooandrean4@users.noreply.github.com>
2021-07-08 10:34:10 +0700

    Revert "Revert "ini controler/soal.php sama views/soal/tambah_soal.php""
    This reverts commit 2e9e9285dc1981bdc2057e5c1820a75b2fdd0228.

--- application/controllers/Soal.php
+++ application/controllers/Soal.php
@@ -20,6 +20,39 @@ class Soal extends CI_Controller {
 		$this->template->load('template', 'soal/soal', $data);
 	}
     public function tambah_Soal(){
+
+		$namamatkul	= $this->input->post('namamatkul');
+		$semester	= $this->input->post('semester');
+		$tanggal	= $this->input->post('tanggal');
+		$dosen		= $this->input->post('dosen');
+		$tahun		= $this->input->post('tahun');
+		$kategori	= $this->input->post('kategori');
+		$file		= $_FILES['file'];
+		if ($file=''){}else{
+			$config['upload_path']	= './views/file';
+			$config['allow_types']	= 'pdf|jpg|png|doc';
+
+			$this->load->library('upload',$config);
+			if(!$this->upload->do_upload('file')){
+				echo"upload gagal"; die();
+			}else{
+				$file=$this->upload->data('file_name');
+			}
+			
+			$data = array(
+				'namamatkul'	=> $namamatkul,
+				'semester'		=> $semester,
+				'tanggal'		=> $tanggal,
+				'dosen'			=> $dosen,
+				'tahun'			=> $tahun,
+				'kategori'		=> $kategori,
+				'file'			=> $file,
+			);
+		}
+
+		// $this->soal_m->input_data($data, 'soal');
+		// redirect('tambah_soal/index');
+
         $query = $this->soal->get();
 		$data = array(
 			'judul' => 'Soal',
@@ -27,5 +60,8 @@ class Soal extends CI_Controller {
 		);
 		$this->template->load('template', 'soal/tambah_soal', $data);
     }
-   
 }
+// 	public function input_soal(){
+		
+   
+// }
